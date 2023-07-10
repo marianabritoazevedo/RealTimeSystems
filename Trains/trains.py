b417a67793e46f4196841ff17dc58f8499c3109b
Deborah Moreira <38132362+deborahmoreira@users.noreply.github.com>
2023-07-10 13:15:35 -0300

    Update trains.py

--- Trains/trains.py
+++ Trains/trains.py
@@ -2,6 +2,8 @@ import threading
 import time
 from constants import *
 
+STEP = 10
+
 # Criação do semáforo
 semaphore = threading.Semaphore(1)
 
@@ -22,21 +24,21 @@ class Train(threading.Thread):
         self.yStart = y
         self.xEnd = self.xStart + 190
         self.yEnd = self.yStart + 190
-        self.speed = 1
+        self.speed = 1.0
         self.event = event
         self.direction = "right"
 
     def run(self):
         while True:
             self.deal_critical()
-            #self.move()
+            self.move()
             if self.event.is_set():
                 break
 
     def setSpeed(self, speed):
         self.speed = speed
 
-    '''def move(self):
+    def move(self):
         if self.x < self.xEnd and self.y == self.yStart:
             self.x += STEP
         elif self.x == self.xEnd and self.y < self.yEnd:
@@ -45,86 +47,86 @@ class Train(threading.Thread):
             self.x -= STEP
         else:
             self.y -= STEP
-        time.sleep(1/self.speed)'''
+        time.sleep(1/self.speed)
 
     def deal_critical(self):
         match self.name:
-            case "T1":
-                # Se T1 estiver entrando na região crítica 1
+            case "trem1":
+                # Se trem1 estiver entrando na região crítica 1
                 if self.x == self.xEnd - train_width and self.y == self.yStart:
                     semaphore.acquire()
                     mutex1.acquire()
                 
-                # Quando T1 sair da região crítica 1
+                # Quando trem1 sair da região crítica 1
                 if self.x == self.xEnd and self.y == self.yEnd:
                     semaphore.release()
                     mutex1.release()
 
-                # Se T1 estiver entrando na região crítica 2
+                # Se trem1 estiver entrando na região crítica 2
                 if self.x == self.xEnd and self.y == self.yEnd - train_width:
                     mutex2.acquire()
                 
-                # Quando T1 sair da região crítica 2
+                # Quando trem1 sair da região crítica 2
                 if self.x == self.xStart and self.y == self.yEnd:
                     mutex2.release()
 
-            case "T2":
-                # Se T2 estiver entrando na região crítica 1
+            case "trem2":
+                # Se trem2 estiver entrando na região crítica 1
                 if self.x == self.xStart + train_width and self.y == self.yEnd:
                     mutex1.acquire()
                 
-                # Quando T2 sair da região crítica 1
+                # Quando trem2 sair da região crítica 1
                 if self.x == self.xStart and self.y == self.yStart:
                     if mutex1.locked():
                         mutex1.release()
                 
-                # Se T2 estiver entrando na região crítica 3
+                # Se trem2 estiver entrando na região crítica 3
                 if self.x == self.xEnd and self.y == self.yEnd - train_width:
                     semaphore.acquire()
                     mutex3.acquire()
 
-                # Quando T2 sair da região crítica 3
+                # Quando trem2 sair da região crítica 3
                 if self.x == self.xStart and self.y == self.yEnd:
                     semaphore.release()
                     mutex3.release()
                 
-            case "T3":
-                # Se T3 estiver entrando na região crítica 2
+            case "trem3":
+                # Se trem3 estiver entrando na região crítica 2
                 if self.x == self.xStart and self.y == self.yStart + train_width:
                     semaphore.acquire()
                     mutex2.acquire()
 
-                # Quando T3 sair da região crítica 2
+                # Quando trem3 sair da região crítica 2
                 if self.x == self.xEnd and self.y == self.yStart:
                     semaphore.release()
                     if mutex2.locked():
                         mutex2.release()
 
-                #  Se T3 estiver entrando na região crítica 4
+                #  Se trem3 estiver entrando na região crítica 4
                 if self.x == self.xEnd -train_width and self.y == self.yStart:
                     mutex4.acquire()
 
-                # Quando T3 sair da região crítica 4
+                # Quando trem3 sair da região crítica 4
                 if self.x == self.xEnd and self.y == self.yEnd:
                     if mutex4.locked():
                         mutex4.release()
                 
-            case "T4":
-                # Se T4 está entrando na região crítica 3
+            case "trem4":
+                # Se trem4 está entrando na região crítica 3
                 if self.x == self.xStart and self.y == self.yStart + train_width:
                     mutex3.acquire()
 
-                # Quando T4 sair da região crítica 3
+                # Quando trem4 sair da região crítica 3
                 if self.x == self.xEnd and self.y == self.yStart:
                     if mutex3.locked():
                         mutex3.release()
 
-                # Se T4 está entrando na região crítica 4
+                # Se trem4 está entrando na região crítica 4
                 if self.x == self.xStart + train_width and self.y == self.yEnd:
                     semaphore.acquire()
                     mutex4.acquire()
                 
-                # Quando T4 sair da região crítica 4
+                # Quando trem4 sair da região crítica 4
                 if self.x == self.xStart and self.y == self.yStart:
                     semaphore.release()
                     if mutex4.locked():
