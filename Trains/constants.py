cb750b054ba5fd9cd7b30fde96bf7ab4a92ecd04
Deborah Moreira <38132362+deborahmoreira@users.noreply.github.com>
2023-07-10 13:17:10 -0300

    Update constants.py

--- Trains/constants.py
+++ Trains/constants.py
@@ -5,10 +5,12 @@ red = (209, 50, 10)
 yellow = (255, 187, 0)
 blue = (39, 129, 219)
 green = (13, 181, 55)
+gray = (128, 128, 128)
+lightgray = (211, 211, 211)
 
 # Definição das dimensões do retângulo da pista
-track_width = 200
-track_height = 200
+track_width = 170
+track_height = 170
 
 # Definição da espessura da linha da borda do retângulo
 border_thickness = 2
@@ -18,5 +20,5 @@ screen_width = 800
 screen_height = 600
 
 # Definição das dimensões dos trens
-train_width = 30
+train_width = 20
 train_height = 20
